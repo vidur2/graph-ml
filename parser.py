ab140b74c1a7bff97cbfc4b64ae1a4bbb952fb5c
vidur2 <vmod2005@gmail.com>
2023-04-30 11:44:35 -0400

    added parser for csv

---
+++ parser.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import pylab as plt
+
+class GraphParser():
+    def __init__(self, path):
+        self.path = path
+        self.graphs = []
+
+    def parseGraphs(self):
+        with open(self.path) as f:
+            for line in f.readlines():
+                g = nx.Graph()
+                split = line.split(",")
+                for i in range(len(split) - 1):
+                    g.add_edge(split[i], split[i + 1])
+                
+                self.graphs.append(g)
+
+
+def test():
+    parser = GraphParser("./dataset/graphs.csv")
+    parser.parseGraphs()
+    for graph in parser.graphs:
+        nx.draw_networkx(graph, with_labels=True)
+        plt.show()
+
+if (__name__ == "__main__"):
+    test()
